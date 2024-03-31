7ae97d70e627b77ccc18b5282e6ad0c27535cfbf
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-03-31 18:16:37 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@ import streamlit as st
 import plotly.express as px
 from service.grap import grap_bar
 from service.grapplotly import grap_plotly
+from service.piegrap impor pie_grap
 
 
 up = st.sidebar.file_uploader('Suba o arquivo', type='csv')
@@ -46,6 +47,9 @@ if up is not None:
         st.subheader('Gráfico de horas por utilização')
         grap_bar(df_select, 'Situação','Horas')
         grap_plotly(df_select, 'Horas','Situação')
+
+        titulo = 'Gráfico de porcentagem - horas por situação'
+        pie_grap(df_select, 'Horas', 'Situação', titulo)
 
 
     
