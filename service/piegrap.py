64b1410727c1c97a1b50e054f21e9aada0d381df
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-03-31 18:25:01 -0300

    Update piegrap.py

--- service/piegrap.py
+++ service/piegrap.py
@@ -9,4 +9,5 @@ def pie_grap(df, value, nomes, titulo:str):
         names = nomes,
         title=titulo
     )
+    fig.update_traces(textposition='inside', textinf='percent+label')
     st.plotly_chart(fig)
