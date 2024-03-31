5cd3e214bea3c1fa72b85fa5bed4c3c6c647a456
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-03-31 18:14:39 -0300

    Add files via upload

---
+++ service/piegrap.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+def pie_grap(df, value, nomes, titulo:str):
+    fig = px.pie(
+        df,
+        values=value,
+        names = nomes,
+        title=titulo
+    )
+    st.plotly_chart(fig)
