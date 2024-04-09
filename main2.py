8c35862af1f3b11ba8d8b7875eda2f62363f3d19
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-04-09 10:49:41 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -51,6 +51,8 @@ if up is not None:
         titulo = 'Gráfico de porcentagem - horas por situação'
         pie_grap(df_select, 'Horas', 'Situação', titulo)
 
+        st.link_button('acess', 'https://newcemapp.streamlit.app/')
+
 
     
     #df_select = df[df['Nome']==name]
