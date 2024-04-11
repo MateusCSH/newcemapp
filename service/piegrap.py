959817d9e2883dc656f888bf644e2db4bb69398b
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-04-11 18:19:04 -0300

    Update piegrap.py

--- service/piegrap.py
+++ service/piegrap.py
@@ -2,13 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-def pie_grap(df, value, nomes, titulo:str):
-    label = nomes
+def pie_grap(df, value, nomes, titulo:str):    
     fig = px.pie(
         df,
         values=value,
         names = nomes,
-        title=titulo,
-        textinfo=label
+        title=titulo       
     )     
     st.plotly_chart(fig)
