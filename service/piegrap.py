865bbd7e76019c4812389f67a055ccd3b1ab2568
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-04-11 18:42:46 -0300

    Update piegrap.py

--- service/piegrap.py
+++ service/piegrap.py
@@ -2,12 +2,15 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-def pie_grap(df, value, nomes, titulo:str):    
+def pie_grap(df, value, nomes, titulo:str):   
+    night_colors = ['rgb(56, 75, 126)', 'rgb(18, 36, 37)', 'rgb(34, 53, 101)',
+                'rgb(36, 55, 57)', 'rgb(6, 4, 4)'] 
     fig = px.pie(
         df,
         values=value,
         names = nomes,
-        title=titulo        
+        title=titulo,
+        color_discrete_sequence=night_colors
     )    
     fig.update_traces(textposition='inside', textinfo='percent+label')
     fig.update_layout(legend=dict(orientation="v", yanchor="bottom", y=0.65, xanchor="right", x=0.85))
